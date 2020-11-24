6f6f8685bc0ba313f967f2ccfac3d77710878afa
Patrick Clausen <201811369@post.au.dk>
2020-11-24 20:57:48 +0100

    Added fixtures, more diagrams

---
+++ Microwave.Test.Integration/Step2Fixture.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace Microwave.Test.Integration
+{
+    [TestFixture]
+    public class Step2Fixture
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
